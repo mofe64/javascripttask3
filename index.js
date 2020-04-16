070239a03a3164377133d4673379b387d2df6652
mofe64 <ogunbiyioladapo33@gmail.com>
2020-04-16 11:55:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const taskFunction = (parmater) => {
+  divisibleNumber = [];
+  for (let i = 0; i < parmater; i++) {
+    divisibleNumber[i] = i + 1;
+    if (
+      divisibleNumber[i] % 2 === 0 &&
+      divisibleNumber[i] % 3 === 0 &&
+      divisibleNumber[i] % 5 === 0
+    )
+      divisibleNumber[i] = "yu-gi-0h";
+    if (divisibleNumber[i] % 2 === 0 && divisibleNumber[i] % 3 === 0)
+      divisibleNumber[i] = "yu-gi";
+    if (divisibleNumber[i] % 2 === 0) divisibleNumber[i] = "yu";
+    if (divisibleNumber[i] % 3 === 0) divisibleNumber[i] = "gi";
+    if (divisibleNumber[i] % 5 === 0) divisibleNumber[i] = "oh";
+  }
+  return divisibleNumber;
+};
+
+taskFunction(30);
+console.log(divisibleNumber);
